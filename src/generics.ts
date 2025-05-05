a4fecafcef785f27a821cebd31045f363cbf9f4e
hasibprangon <hasibulhossainp09@gmail.com>
2025-05-05 10:11:52 +0600

    generics

---
+++ src/generics.ts
@@ -0,0 +1,39 @@
+{
+    // generic type
+
+    type GenericsArray<T> = Array<T>
+
+    // const rollNumber: number[] = [1, 2, 3, 4];
+    const rollNumber: GenericsArray<number> = [1, 2, 3, 4];
+
+    // const name: string[] = ['Hasibul', 'Hossain'];
+    const name: GenericsArray<string> = ['Hasibul', 'Hossain'];
+
+    // const boolArray: boolean[] = [true, false, true]
+    // const boolArray: Array<boolean> = [true, false, true]
+    const boolArray: GenericsArray<boolean> = [true, false, true]
+
+    // array of obj using generics
+
+    const user: GenericsArray<{name: string, age: number}> = [
+        {
+            name: 'Hasib',
+            age: 25
+        },
+        {
+            name:'Prangon',
+            age: 26,
+        }
+    ]
+
+    // generics tuple
+
+    type GenericsTuple<x, y> = [x, y]
+
+    const person: GenericsTuple<string, string> = ['Mr.x', 'Mrs. X'];
+
+    const UserWithID: 
+    GenericsTuple<number, {name:string, email: string}> 
+    = 
+    [1234, {name:'Prangon', email:'a@b.com'}]
+}
